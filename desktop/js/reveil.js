de58c5448aeafc8eb807afe77655e199576f74a8
Halbout Michael <michaelhalbout@gmail.com>
2017-01-03 08:36:22 +0100

    Update reveil.js

--- desktop/js/reveil.js
+++ desktop/js/reveil.js
@@ -1,98 +1,31 @@
-var map;
-var DroitLatLng=new Object();
-var CentreLatLng=new Object();
-var GaucheLatLng=new Object();
-$('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=heliotrope]',function(){
-	$.ajax({
-		type: 'POST',            
-		async: false,
-		url: 'plugins/reveil/core/ajax/reveil.ajax.php',
-		data:{
-			action: 'getInformation',
-			heliotrope:$(this).val(),
-		},
-		dataType: 'json',
-		global: false,
-		error: function(request, status, error) {},
-		success: function(data) {
-			if (!data.result)
-				$('#div_alert').showAlert({message: 'Aucun message reçu', level: 'error'});
-			if (typeof(data.result.geoloc) !== 'undefined') {
-				var center=data.result.geoloc.configuration.coordinate.split(",");
-				CentreLatLng.lat=parseFloat(center[0]);
-				CentreLatLng.lng=parseFloat(center[1]);
-				// création de la carte
-				$('#MyMap').show();
-				map = new google.maps.Map( document.getElementById('MyMap'),{
-					'mapTypeControl':  true,
-					'streetViewControl': false,
-					'panControl':true,
-					'scaleControl': true,
-					'overviewMapControl': true,
-					'mapTypeId': google.maps.MapTypeId.ROADMAP,
-					'center': CentreLatLng,
-					'scrollwheel': true,
-					'zoom': 20
-				});
-			}
-		}
-	});
-});
-$('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=TypeGestion]',function(){
+$('body').on('change','.eqLogicAttr[data-l1key=configuration][data-l2key=ReveilType]',function(){
 	switch($(this).val()){
-		case 'DayNight':
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisEval]').parent().parent().show();
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisDay]').parent().parent().show();
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisNight]').parent().parent().show();
-			$('.AngleSoleil').show();
+		case 'DawnSimulatorEngine':
+			$('.DawnSimulatorEngine').show();
 		break;
-		case 'Helioptrope':
-		case 'Other':
-			$('.AngleSoleil').hide();
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisEval]').parent().parent().hide();
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisDay]').parent().parent().hide();
-			$('.eqLogicAttr[data-l1key=configuration][data-l2key=DelaisNight]').parent().parent().hide();
+		default:
+			$('.DawnSimulatorEngine').hide();
 		break;
 	}
 });
-function getAngle(Coordinates) {
-		var longDelta = Coordinates[1].lng - Coordinates[0].lng;
-		var y = Math.sin(longDelta) * Math.cos(Coordinates[1].lat);
-		var x = Math.cos(Coordinates[0].lat)*Math.sin(Coordinates[1].lat) - Math.sin(Coordinates[0].lat)*Math.cos(Coordinates[1].lat)*Math.cos(longDelta);
-
-		var radians = Math.atan2(y, x);
-		var angle = radians * 180 / Math.PI
-		while (angle < 0) {
-			angle += 360;
-		}
-		angle=angle % 360;
-		angle=angle-90;
-		return  angle;
-	}
-
 function saveEqLogic(_eqLogic) {
 	var state_order = '';
     if (!isset(_eqLogic.configuration)) {
         _eqLogic.configuration = {};
     }	
 	if (typeof( _eqLogic.cmd) !== 'undefined') {
-			_eqLogic.configuration.condition=new Object();
-			_eqLogic.configuration.action=new Object();
+			_eqLogic.configuration.Conditions=new Object();
+			_eqLogic.configuration.Equipements=new Object();
 			var ConditionArray= new Array();
-			var OpenArray= new Array();
-			var CloseArray= new Array();
+			var EquipementArray= new Array();
 			$('#tab_condition .ConditionGroup').each(function( index ) {
 				ConditionArray.push($(this).getValues('.expressionAttr')[0])
 			});
-			$('#tab_ouverture .ActionGroup').each(function( index ) {
-				OpenArray.push($(this).getValues('.expressionAttr')[0])
+			$('#tab_equipement .ActionGroup').each(function( index ) {
+				EquipementArray.push($(this).getValues('.expressionAttr')[0])
 			});
-			$('#tab_fermeture .ActionGroup').each(function( index ) {
-				CloseArray.push($(this).getValues('.expressionAttr')[0])
-			});
-			_eqLogic.configuration.condition=ConditionArray;
-			_eqLogic.configuration.action.open=OpenArray;
-			_eqLogic.configuration.action.close=CloseArray;
+			_eqLogic.configuration.Conditions=ConditionArray;
+			_eqLogic.configuration.Equipements=EquipementArray;
 	}
    	return _eqLogic;
 }
@@ -103,97 +36,18 @@ function printEqLogic(_eqLogic) {
 	$('.eqLogicAttr[data-l1key=configuration][data-l2key=Centre]').val(JSON.stringify(_eqLogic.configuration.Centre));
 	$('.eqLogicAttr[data-l1key=configuration][data-l2key=Gauche]').val(JSON.stringify(_eqLogic.configuration.Gauche));
 	TraceMapZone(_eqLogic);
-	if (typeof(_eqLogic.configuration.condition) !== 'undefined') {
-		for(var index in _eqLogic.configuration.condition) { 
-			if( (typeof _eqLogic.configuration.condition[index] === "object") && (_eqLogic.configuration.condition[index] !== null) )
-				addCondition(_eqLogic.configuration.condition[index],  '{{Condition}}',$('#tab_condition').find('.div_Condition'));
+	if (typeof(_eqLogic.configuration.Conditions) !== 'undefined') {
+		for(var index in _eqLogic.configuration.Conditions) { 
+			if( (typeof _eqLogic.configuration.Conditions[index] === "object") && (_eqLogic.configuration.Conditions[index] !== null) )
+				addCondition(_eqLogic.configuration.Conditions[index],  '{{Condition}}',$('#tab_condition').find('.div_Condition'));
 		}
 	}
-	if (typeof(_eqLogic.configuration.action) !== 'undefined') {
-		if (typeof(_eqLogic.configuration.action.open) !== 'undefined') {
-			for(var index in _eqLogic.configuration.action.open) { 
-				if( (typeof _eqLogic.configuration.action.open[index] === "object") && (_eqLogic.configuration.action.open[index] !== null) )
-					addAction(_eqLogic.configuration.action.open[index],  '{{Action}}',$('#tab_ouverture').find('.div_action'));
-			}
-		}
-		if (typeof(_eqLogic.configuration.action.close) !== 'undefined') {
-			for(var index in _eqLogic.configuration.action.close) { 
-				if( (typeof _eqLogic.configuration.action.close[index] === "object") && (_eqLogic.configuration.action.close[index] !== null) )
-					addAction(_eqLogic.configuration.action.close[index],  '{{Action}}',$('#tab_fermeture').find('.div_action'));
-			}
+	if (typeof(_eqLogic.configuration.Equipements) !== 'undefined') {
+		for(var index in _eqLogic.configuration.Equipements) { 
+			if( (typeof _eqLogic.configuration.Equipements[index] === "object") && (_eqLogic.configuration.Equipements[index] !== null) )
+				addAction(_eqLogic.configuration.Equipements[index],  '{{Action}}',$('#tab_ouverture').find('.div_action'));
 		}
 	}	
-}
-function TraceMapZone(_zone){
-	DroitLatLng.lat=CentreLatLng.lat;
-	DroitLatLng.lng=CentreLatLng.lng- (1 / 3600);
-	GaucheLatLng.lat=CentreLatLng.lat;
-	GaucheLatLng.lng=CentreLatLng.lng+ (1 / 3600);
-	if (typeof(_zone.configuration.Droite) !== 'undefined' && _zone.configuration.Droite != "" ) {
-		DroitLatLng.lat=parseFloat(_zone.configuration.Droite.lat);
-		DroitLatLng.lng=parseFloat(_zone.configuration.Droite.lng);
-	}
-	if (typeof(_zone.configuration.Centre) !== 'undefined' && _zone.configuration.Centre != "" ) {
-		CentreLatLng.lat=parseFloat(_zone.configuration.Centre.lat);
-		CentreLatLng.lng=parseFloat(_zone.configuration.Centre.lng);
-	}
-	if (typeof(_zone.configuration.Gauche) !== 'undefined' && _zone.configuration.Gauche != "") {
-		GaucheLatLng.lat=parseFloat(_zone.configuration.Gauche.lat);
-		GaucheLatLng.lng=parseFloat(_zone.configuration.Gauche.lng);
-	}
-	var Droit=new google.maps.Marker({
-		position: DroitLatLng,
-		map: map,
-		draggable:true,
-		title: _zone.name + " - Droite vue extérieur"
-	  });
-	var Centre=new google.maps.Marker({
-		position: CentreLatLng,
-		map: map,
-		draggable:true,
-		title: _zone.name + " - Centre de l'angle d'ouverture"
-	  });
-	var Gauche=new google.maps.Marker({
-		position: GaucheLatLng,
-		map: map,
-		draggable:true,
-		title: _zone.name  + " - Gauche vue extérieur"
-	  });
-	var PolylineDroite =new google.maps.Polyline({
-		path: [CentreLatLng,DroitLatLng],
-		geodesic: true,
-		strokeColor: '#40A497',
-		strokeOpacity: 1.0,
-		map: map,
-		strokeWeight: 2
-	});
-	var PolylineGauche =new google.maps.Polyline({
-		path: [GaucheLatLng,CentreLatLng],
-		geodesic: true,
-		strokeColor: '#40A497',
-		strokeOpacity: 1.0,
-		map: map,
-		strokeWeight: 2
-	});
-	google.maps.event.addListener(Droit,'drag', function(event) {
-		DroitLatLng.lat=event.latLng.lat();
-		DroitLatLng.lng=event.latLng.lng();
-		$('.eqLogicAttr[data-l1key=configuration][data-l2key=Droite]').val(JSON.stringify(event.latLng));
-		PolylineDroite.setPath([CentreLatLng,DroitLatLng]);
-	});
-	google.maps.event.addListener(Centre,'drag', function(event) {
-		CentreLatLng.lat=event.latLng.lat();
-		CentreLatLng.lng=event.latLng.lng();
-		$('.eqLogicAttr[data-l1key=configuration][data-l2key=Centre]').val(JSON.stringify(event.latLng));
-		PolylineGauche.setPath([GaucheLatLng,CentreLatLng]);
-		PolylineDroite.setPath([CentreLatLng,DroitLatLng]);
-	});
-	google.maps.event.addListener(Gauche,'drag', function(event) {
-		GaucheLatLng.lat=event.latLng.lat();
-		GaucheLatLng.lng=event.latLng.lng();
-		$('.eqLogicAttr[data-l1key=configuration][data-l2key=Gauche]').val(JSON.stringify(event.latLng));
-		PolylineGauche.setPath([GaucheLatLng,CentreLatLng]);
-	});
 }
 function addCondition(_action, _name, _el) {
 	if (!isset(_action)) {
