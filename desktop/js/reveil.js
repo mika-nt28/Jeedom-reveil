fb5572744b43fe23798861c918de33d21714dbcb
Halbout Michael <michaelhalbout@gmail.com>
2017-01-05 08:23:59 +0100

    Update reveil.js

--- desktop/js/reveil.js
+++ desktop/js/reveil.js
@@ -257,3 +257,9 @@ $('body').on( 'click','.bt_selectCmdExpression', function() {
 		$(_this).closest('.input-group').find('.cmdAttr').val(result.human);
 	});
 });  
+$('body').on('click','.ScheduleCron',function(){
+  var el = $(this).closest('.input-group').find('.eqLogicAttr');
+  jeedom.getCronSelectModal({},function (result) {
+    el.value(result.value);
+  });
+});
