8925ad06eff7b4c8ffd4704fc77c8c0b9f1b610a
Halbout Michael <michaelhalbout@gmail.com>
2017-05-10 13:28:38 +0200

    Update reveil.php

--- desktop/php/reveil.php
+++ desktop/php/reveil.php
@@ -48,165 +48,172 @@ $eqLogics = eqLogic::byType('reveil');
 			?>
 		</div>
 	</div>  
-	<div class="col-lg-10 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-		<form class="form-horizontal">
-			<fieldset>		
-				<legend>
-					<i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  
-					<i class="fa fa-cogs eqLogicAction pull-right cursor expertModeVisible" data-action="configure"></i>
-					<a class="btn btn-default btn-xs pull-right expertModeVisible eqLogicAction" data-action="copy"><i class="fa fa-copy"></i>{{Dupliquer}}</a>
-					<a class="btn btn-success btn-xs eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
-					<a class="btn btn-danger btn-xs eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
-				</legend> 
-			</fieldset> 
-		</form>	
-		<div class="row" style="padding-left:25px;">
-			<ul class="nav nav-tabs" id="tab_zones">	
-				<li class="active"><a href="#tab_general"><i class="fa fa-cogs"></i> {{Général}}</a></li>
-				<li class="SelectMap"><a href="#tab_map"><i class="fa fa-map"></i> {{Afficher la carte}}</a></li>
-				<li><a href="#tab_condition"><i class="fa fa-pencil"></i> {{Conditions d'exécution}}</a></li>
-				<li><a href="#tab_action"><i class="fa fa-pencil"></i> {{Actions}}</a></li>
-				<li><a href="#tab_cmd"><i class="fa fa-pencil"></i> {{Commandes du plugin}}</a></li>
-			</ul>
-			<div class="tab-content TabCmdZone">
-				<div class="tab-pane active" id="tab_general">
-					<form class="form-horizontal">
-						<fieldset>
-							<div class="form-group ">
-								<label class="col-sm-2 control-label">{{Nom du reveil}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Indiquer le nom de votre réveil" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-5">
-									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du groupe de zones}}"/>
-								</div>
+	<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+		<a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> Sauvegarder</a>
+		<a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> Supprimer</a>
+		<a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> Configuration avancée</a>
+		<a class="btn btn-default eqLogicAction pull-right expertModeVisible " data-action="copy"><i class="fa fa-copy"></i>{{Dupliquer}}</a>
+		<ul class="nav nav-tabs" role="tablist">
+			<li role="presentation">
+				<a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay">
+					<i class="fa fa-arrow-circle-left"></i>
+				</a>
+			</li>
+			<li role="presentation" class="active">
+				<a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab" aria-expanded="true">
+					<i class="fa fa-tachometer"></i> Equipement</a>
+			</li>
+			<li role="presentation" class="">
+				<a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
+					<i class="fa fa-list-alt"></i> Commandes</a>
+			</li>
+			<li role="presentation" class="">
+				<a href="#conditiontab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
+					<i class="fa fa-list-alt"></i> {{Conditions d'exécution}}</a>
+			</li>
+			<li role="presentation" class="">
+				<a href="#actiontab" aria-controls="profile" role="tab" data-toggle="tab" aria-expanded="false">
+					<i class="fa fa-list-alt"></i> {{Actions}}</a>
+			</li>
+		</ul>
+		<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
+				<form class="form-horizontal">
+					<fieldset>
+						<div class="form-group ">
+							<label class="col-sm-2 control-label">{{Nom du reveil}}
+								<sup>
+									<i class="fa fa-question-circle tooltips" title="Indiquer le nom de votre réveil" style="font-size : 1em;color:grey;"></i>
+								</sup>
+							</label>
+							<div class="col-sm-5">
+								<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+								<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du groupe de zones}}"/>
 							</div>
-							<div class="form-group">
-								<label class="col-sm-2 control-label" >{{Objet parent}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Indiquer l'objet dans lequel le widget de ce réveil apparaîtra sur le dashboard" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-5">
-									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-										<option value="">{{Aucun}}</option>
-										<?php
-											foreach (object::all() as $object) 
-												echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-										?>
-									</select>
-								</div>
-							</div>
-							<div class="form-group">
-								<label class="col-md-2 control-label">
-									{{Catégorie}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Choisissez une catégorie
-									Cette information n'est pas obligatoire mais peut être utile pour filtrer les widgets" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-md-8">
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label" >{{Objet parent}}
+								<sup>
+									<i class="fa fa-question-circle tooltips" title="Indiquer l'objet dans lequel le widget de ce réveil apparaîtra sur le dashboard" style="font-size : 1em;color:grey;"></i>
+								</sup>
+							</label>
+							<div class="col-sm-5">
+								<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+									<option value="">{{Aucun}}</option>
 									<?php
-									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-										echo '<label class="checkbox-inline">';
-										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-										echo '</label>';
-									}
+										foreach (object::all() as $object) 
+											echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 									?>
+								</select>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">
+								{{Catégorie}}
+								<sup>
+									<i class="fa fa-question-circle tooltips" title="Choisissez une catégorie
+								Cette information n'est pas obligatoire mais peut être utile pour filtrer les widgets" style="font-size : 1em;color:grey;"></i>
+								</sup>
+							</label>
+							<div class="col-md-8">
+								<?php
+								foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+									echo '<label class="checkbox-inline">';
+									echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+									echo '</label>';
+								}
+								?>
 
-								</div>
 							</div>
-							<div class="form-group">
-								<label class="col-sm-2 control-label" >
-									{{Etat du widget}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Choisissez les options de visibilité et d'activation
-									Si l'équipement n'est pas activé il ne sera pas utilisable dans Jeedom, mais visible sur le dashboard
-									Si l'équipement n'est pas visible il sera caché sur le dashboard, mais utilisable dans Jeedom" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-5">
-									<label>{{Activer}}</label>
-									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable"/>
-									<label>{{Visible}}</label>
-									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible"/>
-								</div>
-							</div><div class="form-group">
-								<label class="col-sm-2 control-label" >
-									{{Jours Ferier}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Si actif le reveil sera ignorer les jours feriés" style="font-size : 1em;color:grey;"></i>
-									</sup>
-								</label>
-								<div class="col-sm-5">
-									<label>{{Ignorer}}</label>
-									<input type="checkbox" class="eqLogicAttr" data-label-text="{{Ignorer}}" data-l1key="isHolidays"/>
-									</div>
-							</div>
-							<div class="form-group">
-								<label class="col-sm-2 control-label">{{Configurer l'heure du reveil}}
-									<sup>
-										<i class="fa fa-question-circle tooltips" title="Configurer l'heure du réveil"></i>
-									</sup>
-								</label>
-								<div class=" col-sm-5 input-group">
-									<input class="eqLogicAttr form-control input-sm" data-l1key="configuration"  data-l2key="ScheduleCron" placeholder="{{Choisir à l'aide de l'assistant, l'heure de départ de votre réveil}}">
-									<span class="input-group-btn">
-										<a class="btn btn-success btn-sm ScheduleCron" >
-											<i class="fa fa-list-alt"></i>
-										</a>
-									</span>
-								</div>
-							</div>
-						</fieldset>
-					</form>
-				</div>	
-				<div class="tab-pane" id="tab_condition">
-					<form class="form-horizontal">
-						<fieldset>
-							<legend>{{Les conditions d'execution :}}
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label" >
+								{{Etat du widget}}
 								<sup>
-									<i class="fa fa-question-circle tooltips" title="Saisir toutes les conditions d'exécution de la gestion"></i>
+									<i class="fa fa-question-circle tooltips" title="Choisissez les options de visibilité et d'activation
+								Si l'équipement n'est pas activé il ne sera pas utilisable dans Jeedom, mais visible sur le dashboard
+								Si l'équipement n'est pas visible il sera caché sur le dashboard, mais utilisable dans Jeedom" style="font-size : 1em;color:grey;"></i>
 								</sup>
-								<a class="btn btn-success btn-xs conditionAttr" data-action="add" style="margin-left: 5px;">
-									<i class="fa fa-plus-circle"></i>
-									{{Ajouter Condition}}
-								</a>
-							</legend>
-							<div class="div_Condition"></div>
-						</fieldset>
-					</form>
-				</div>				
-				<div class="tab-pane" id="tab_action">
-					<form class="form-horizontal">
-						<fieldset>
-							<legend>{{Liste des equipements a executé:}}
+							</label>
+							<div class="col-sm-5">
+								<label>{{Activer}}</label>
+								<input type="checkbox" class="eqLogicAttr" data-label-text="{{Activer}}" data-l1key="isEnable"/>
+								<label>{{Visible}}</label>
+								<input type="checkbox" class="eqLogicAttr" data-label-text="{{Visible}}" data-l1key="isVisible"/>
+							</div>
+						</div><div class="form-group">
+							<label class="col-sm-2 control-label" >
+								{{Jours Ferier}}
 								<sup>
-									<i class="fa fa-question-circle tooltips" title="Saisir tous les équipements à exécuter"></i>
+									<i class="fa fa-question-circle tooltips" title="Si actif le reveil sera ignorer les jours feriés" style="font-size : 1em;color:grey;"></i>
 								</sup>
-								<a class="btn btn-success btn-xs ActionAttr" data-action="add" style="margin-left: 5px;">
-									<i class="fa fa-plus-circle"></i>
-									{{Ajouter un equipement}}
-								</a>
-							</legend>
-							<div class="div_action"></div>
-						</fieldset>
-					</form>
-				</div>		
-				<div class="tab-pane " id="tab_cmd">	
-					<table id="table_cmd" class="table table-bordered table-condensed">
-					    <thead>
-						<tr>
-						    <th>Nom</th>
-						    <th>Paramètre</th>
-						</tr>
-					    </thead>
-					    <tbody></tbody>
-					</table>
-				</div>	
-			</div>
+							</label>
+							<div class="col-sm-5">
+								<label>{{Ignorer}}</label>
+								<input type="checkbox" class="eqLogicAttr" data-label-text="{{Ignorer}}" data-l1key="isHolidays"/>
+								</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-2 control-label">{{Configurer l'heure du reveil}}
+								<sup>
+									<i class="fa fa-question-circle tooltips" title="Configurer l'heure du réveil"></i>
+								</sup>
+							</label>
+							<div class=" col-sm-5 input-group">
+								<input class="eqLogicAttr form-control input-sm" data-l1key="configuration"  data-l2key="ScheduleCron" placeholder="{{Choisir à l'aide de l'assistant, l'heure de départ de votre réveil}}">
+								<span class="input-group-btn">
+									<a class="btn btn-success btn-sm ScheduleCron" >
+										<i class="fa fa-list-alt"></i>
+									</a>
+								</span>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>	
+			<div role="tabpanel" class="tab-pane" id="conditiontab">
+				<form class="form-horizontal">
+					<fieldset>
+						<legend>{{Les conditions d'execution :}}
+							<sup>
+								<i class="fa fa-question-circle tooltips" title="Saisir toutes les conditions d'exécution de la gestion"></i>
+							</sup>
+							<a class="btn btn-success btn-xs conditionAttr" data-action="add" style="margin-left: 5px;">
+								<i class="fa fa-plus-circle"></i>
+								{{Ajouter Condition}}
+							</a>
+						</legend>
+						<div class="div_Condition"></div>
+					</fieldset>
+				</form>
+			</div>				
+			<div role="tabpanel" class="tab-pane" id="actiontab">
+				<form class="form-horizontal">
+					<fieldset>
+						<legend>{{Liste des equipements a executé:}}
+							<sup>
+								<i class="fa fa-question-circle tooltips" title="Saisir tous les équipements à exécuter"></i>
+							</sup>
+							<a class="btn btn-success btn-xs ActionAttr" data-action="add" style="margin-left: 5px;">
+								<i class="fa fa-plus-circle"></i>
+								{{Ajouter un equipement}}
+							</a>
+						</legend>
+						<div class="div_action"></div>
+					</fieldset>
+				</form>
+			</div>		
+			<div role="tabpanel" class="tab-pane" id="commandtab">	
+				<table id="table_cmd" class="table table-bordered table-condensed">
+					<thead>
+					<tr>
+						<th>Nom</th>
+						<th>Paramètre</th>
+					</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>	
 		</div>
 	</div>
 </div>
