ef26f73c5e1b2133c70e967cb75f0ff1727fe57f
Halbout Michael <michaelhalbout@gmail.com>
2017-05-29 12:27:20 +0200

    Update reveil.php

--- desktop/php/reveil.php
+++ desktop/php/reveil.php
@@ -188,23 +188,19 @@ $eqLogics = eqLogic::byType('reveil');
 									<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="Schedule" data-l3key="Dimanche" />
 									{{Dimanche}}
 								</label>
-								<label class="checkbox-inline">
-									<select class="eqLogicAttr" data-l1key="configuration" data-l2key="Schedule" data-l3key="Heure" >
-										<?php
-											for($loop=0; $loop<24; $loop++){
-												echo'<option value="'.$loop.'">'.$loop.'</option>';
-										?>
-									</select>
-								</label>
-								<label class="checkbox-inline">
-									<select class="eqLogicAttr" data-l1key="configuration" data-l2key="Schedule" data-l3key="Minute" >
-										<?php
-											for($loop=0; $loop<60;$loop++){
-												echo'<option value="'.$loop.'">'.$loop.'</option>';
-											}
-										?>
-									</select>
-								</label>
+								<select class="eqLogicAttr" data-l1key="configuration" data-l2key="Schedule" data-l3key="Heure" >
+									<?php
+										for($loop=0; $loop<24; $loop++){
+											echo'<option value="'.$loop.'">'.$loop.'</option>';
+									?>
+								</select>
+								<select class="eqLogicAttr" data-l1key="configuration" data-l2key="Schedule" data-l3key="Minute" >
+									<?php
+										for($loop=0; $loop<60;$loop++){
+											echo'<option value="'.$loop.'">'.$loop.'</option>';
+										}
+									?>
+								</select>
 							</div>
 						</div>
 					</fieldset>
