e6cb40dd4a0a5f5a879354cba7ac65e6c5496b61
Halbout Michael <michaelhalbout@gmail.com>
2017-05-29 14:02:42 +0200

    Create install.php

---
+++ plugin_info/install.php
@@ -0,0 +1,19 @@
+<?php
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+function reveil_update() {
+  log::add('reveil','debug','Lancement du scripte de mise a  jours des Flags'); 
+   foreach(eqLogic::byType('reveil') as $eqLogic){ 
+    if($eqLogic->getConfiguration('ScheduleCron') != ''){
+      $Shedule=explode(' ',$eqLogic->getConfiguration('ScheduleCron'));
+      $Config['Heure']=$Shedule[1];
+      $Config['Minute']=$Shedule[0];
+      foreach(explode(',',$Shedule[4]) as $day){
+        $Config[$day]=true;
+      }
+      $eqLogic->setConfiguration('Schedule',$Config);
+      $eqLogic->setConfiguration('ScheduleCron','');
+      $eqLogic->save();
+    }
+	}
+}
+?>
