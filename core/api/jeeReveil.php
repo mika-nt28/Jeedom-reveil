c65929525e78ba83b1bf31c6b5a1b33009a81560
Halbout Michael <michaelhalbout@gmail.com>
2017-12-05 08:53:08 +0100

    Update jeeReveil.php

--- core/api/jeeReveil.php
+++ core/api/jeeReveil.php
@@ -9,22 +9,22 @@ if (!jeedom::apiAccess(init('apikey'), 'reveil')) {
 
 $content = file_get_contents('php://input');
 $json = json_decode($content, true);
-log::add('geotrav', 'debug', $content);
+log::add('reveil', 'debug', $content);
 
 $eqlogic = reveil::byId(init('id'));
 if (!is_object($eqlogic)) {
-	throw new Exception(__('Commande ID geotrav inconnu : ', __FILE__) . init('id'));
+	throw new Exception(__('Commande ID reveil inconnu : ', __FILE__) . init('id'));
 }
 if ($eqlogic->getEqType_name() != 'reveil') {
 	throw new Exception(__('Cette commande n\'est pas de type reveil : ', __FILE__) . init('id'));
 }
-if (is_array($json) && isset($json['reveil'])) {
-		$ConigSchedule=$eqlogic->getConfiguration('Programation');
-    $ConigSchedule[0]["Heure"]=$json['Heure'];
-    $ConigSchedule[0]["Minute"]=$json['Minute'];
-    $eqlogic->setConfiguration('Programation',$ConigSchedule);
-    $eqlogic->save();
-    $eqlogic->NextStart();
+if (is_array($json)) {
+	$ConigSchedule=$eqlogic->getConfiguration('Programation');
+	$ConigSchedule[0]["Heure"]=$json['heure'];
+	$ConigSchedule[0]["Minute"]=$json['minute'];
+	$eqlogic->setConfiguration('Programation',$ConigSchedule);
+	$eqlogic->save();
+	$eqlogic->NextStart();
 }
 return true;
 ?>
