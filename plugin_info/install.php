821e1699307ae12ef39d3dfac4b7012c32a61a15
Halbout Michael <michaelhalbout@gmail.com>
2018-02-01 11:34:26 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,12 +5,6 @@ function reveil_install(){
 function reveil_update(){
 	log::add('reveil','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('reveil') as $eqLogic){
-		if(isset($eqLogic->getConfiguration('Schedule')))
-			$eqLogic->setConfiguration('Schedule','');
-		if(isset($eqLogic->getConfiguration('programation')))
-			$eqLogic->setConfiguration('programation','');
-		if(isset($eqLogic->getConfiguration('url')))
-			$eqLogic->setConfiguration('url','');
 		$eqLogic->save();
 	}
 	log::add('reveil','debug','Fin du script de mise a jours');
